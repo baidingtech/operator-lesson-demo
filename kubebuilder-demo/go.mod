4fe4a7d1230069b79a2902398e70575a7ec5fde5
wangtaotao <wangtaotao2015@163.com>
2022-06-11 13:41:09 +0800

    init webhook

--- kubebuilder-demo/go.mod
+++ kubebuilder-demo/go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/gomega v1.18.1
+	k8s.io/api v0.24.0
 	k8s.io/apimachinery v0.24.0
 	k8s.io/client-go v0.24.0
 	sigs.k8s.io/controller-runtime v0.12.0
--- kubebuilder-demo/main.go
+++ kubebuilder-demo/main.go
@@ -85,6 +85,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "App")
 		os.Exit(1)
 	}
+	if err = (&ingressv1beta1.App{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "App")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
